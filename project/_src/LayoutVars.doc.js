9065bdb496d920127013e74f21f41369b3306b05
davidmars <d.marsalone@gmail.com>
2020-01-24 12:05:15 +0100

    LayoutVars.doc.js

---
+++ project/_src/LayoutVars.doc.js
@@ -0,0 +1,12 @@
+alert("Ce fichier n'est pas à inclure dans le projet, il sert juste à docummenter");
+window.LayoutVars={
+    rootUrl: "/github/manifestory/fr",
+    fmkHttpRoot: "/github/manifestory",
+    googleAnalyticsId: null,
+    translations: {
+        "pwa: install message": "Souhaitez-vous installer l'application?"
+    },
+    isModeDev: false,
+    isExportStatic: false,
+    wysiwyg: false
+};
