a58c869940a1784dac0a3834a270351f7d9a58ba
davidmars <d.marsalone@gmail.com>
2020-02-03 11:16:22 +0100

    bubulles html random

--- project/_src/bubulles/BubullesHtml.js
+++ project/_src/bubulles/BubullesHtml.js
@@ -2,18 +2,40 @@ require("./bubulles.less");
 
 export default class BubullesHtml{
     constructor() {
-
+        this.blue=[
+            "blue",
+            "blue-2",
+            "blue-3"
+        ];
+        this.orange=[
+            "orange",
+            "orange-1",
+            "orange-2",
+            "orange-3",
+            "orange-4",
+            "orange-5",
+        ];
     }
 
     fromDom(){
+        let me=this;
         $("[bubulles-html]").not("[bubulles-html-init]").each(function(){
             let $el=$(this);
             $el.attr("bubulles-html-init","1");
             let color=$el.closest("[color-theme]").attr("color-theme");
             $el.text(color);
             let svgUrl=null;
-            if(color){
-                svgUrl=`${LayoutVars.fmkHttpRoot}/project/_src/bubulles/svg/${color}.svg`
+            let file=null;
+            switch (color) {
+                case "orange":
+                    file=utils.array.randomEntry(me.orange);
+                    break;
+                case "blue":
+                    file=utils.array.randomEntry(me.blue);
+                    break;
+            }
+            if(file){
+                svgUrl=`${LayoutVars.fmkHttpRoot}/project/_src/bubulles/svg/${file}.svg`
             }
             if(svgUrl){
                 let $img=$("<img src='' alt=''/>");
