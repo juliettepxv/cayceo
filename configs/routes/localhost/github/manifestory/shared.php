72dd8ce8dbbf9a3eb138cc281f0fc2cf3c48f4aa
david <d.marsalone@gmail.com>
2020-02-03 06:19:03 +0100

    config traductions google sheet

--- configs/routes/localhost/github/manifestory/shared.php
+++ configs/routes/localhost/github/manifestory/shared.php
@@ -7,9 +7,8 @@ the()->project->languagesUrls=[
 //force https?
 the()->configProjectUrl->forceHttps=false;
 // Traductions ui.
-// https://docs.google.com/spreadsheets/d/1kefhXt0Z7g0P2cGItQK4k_hqhSoFTMaJyvzNYjKeqw8
-// Copiez-collez ce google Sheet pour pouvoir le modifier puis modifiez l'url. Vous pouvez bien entendu utiliser un csv en local mais c'est moins pratique :)
-the()->project->config_translations_csv_url="https://docs.google.com/spreadsheets/d/1kefhXt0Z7g0P2cGItQK4k_hqhSoFTMaJyvzNYjKeqw8/export?gid=0&format=csv";
+// trads manifestory only
+the()->project->config_translations_csv_url="https://docs.google.com/spreadsheets/d/1RHoObPPW-YId5Az-VuAsEllszPMh5n_NB170BaUkIfg/export?gid=0&format=csv";
 the()->project->config_translations_debug=true; //quand true recharge à chaque fois le CSV
 
 //config options
--- project/_src/components/next.php
+++ project/_src/components/next.php
@@ -1,9 +1,14 @@
 <?php
 /** @var \Classiq\Models\Project $vv */
 use Classiq\Models\Project;
+
 ?>
 <div class="next-one">
     <a href="<?=$vv->next()->href()?>">
-        <?=trad("projet suivant")?>
+        <?if($vv->isNews()):?>
+            <?=trad("news suivante")?>
+        <?else:?>
+            <?=trad("projet suivant")?>
+        <?endif;?>
     </a>
 </div>
