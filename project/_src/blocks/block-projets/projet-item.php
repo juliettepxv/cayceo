80c59f9026d1ea0e5ea0696913bfff87cbc72789
Juliette Salomon <juliette.salomon@gmail.com>
2020-02-03 10:39:06 +0100

    css - projet-item + typo

--- project/_src/blocks/block-projets/projet-item.php
+++ project/_src/blocks/block-projets/projet-item.php
@@ -1,49 +1,51 @@
 <?php
+
 use Classiq\Models\JsonModels\ListItem;
+
 /** @var ListItem $vv */
 
 /** @var \Classiq\Models\Project $projet */
-$projet=$vv->targetUid(true);
-$img=null;
-$text="...";
-if($projet){
-    $img=$projet->thumbnail(true);
-    $text=$projet->name;
-    if($projet->titre_lang){
-        $text=$projet->titre_lang;
+$projet = $vv->targetUid(true);
+$img = null;
+$text = "...";
+if ($projet) {
+    $img = $projet->thumbnail(true);
+    $text = $projet->name;
+    if ($projet->titre_lang) {
+        $text = $projet->titre_lang;
     }
 }
-$css=$img?"has-img":"";
+$css = $img ? "has-img" : "";
 
 
 ?>
-<?if($projet || cq()->wysiwyg()):?>
+<? if ($projet || cq()->wysiwyg()): ?>
 
-    <div class="projet-item <?=$css?>"
-        scroll-active=""
-        dss="<?=rand(50,100)/100?>"
-        <?=$vv->wysiwyg()->attr()?>
-         <?if($projet):?>href="<?=$projet->href()?>"<?endif;?>
+    <div class="projet-item <?= $css ?>"
+         scroll-active=""
+         dss="<?= rand(50, 100) / 100 ?>"
+        <?= $vv->wysiwyg()->attr() ?>
+         <? if ($projet): ?>href="<?= $projet->href() ?>"<? endif; ?>
     >
 
-        <?if($img):?>
-            <img src="<?=$img->httpPath()?>" width="<?=$img->image_width?>" height="<?=$img->image_height?>">
-        <?endif;?>
+        <? if ($img): ?>
+            <img src="<?= $img->httpPath() ?>" width="<?= $img->image_width ?>" height="<?= $img->image_height ?>">
+        <? endif; ?>
 
-        <?if($projet):?>
-        <div class="texts">
-            <h2><?=$text?></h2>
-            <a class="button naked sz-normal px-none" href="<?=$projet->href()?>">
-                Découvrir
-                <?=pov()->svg->use("startup-caret-right")?>
-            </a>
-        </div>
-        <?endif;?>
+        <? if ($projet): ?>
+            <div class="texts">
+                <h2><?= $text ?></h2>
+                <a class="button naked sz-normal px-none" href="<?= $projet->href() ?>">
+                    Découvrir
+                    <?= pov()->svg->use("startup-caret-right") ?>
+                </a>
+            </div>
+        <? endif; ?>
 
-        <?if($vv->getData("decoAfter")):?>
-        <div class="deco" dom-copy="bubulle">bubulles ici</div>
-        <?endif?>
+        <? if ($vv->getData("decoAfter")): ?>
+            <div class="deco" dom-copy="bubulle">bubulles ici</div>
+        <? endif ?>
 
     </div>
 
-<?endif?>
+<? endif ?>
