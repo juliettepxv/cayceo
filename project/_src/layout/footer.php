9487a46b4299d9939a64a82c6d59fb332e8cad07
david <d.marsalone@gmail.com>
2020-02-06 16:25:30 +0100

    footer logos + next one centré

--- project/_src/layout/footer.php
+++ project/_src/layout/footer.php
@@ -2,5 +2,21 @@
 
 ?>
 <footer id="footer">
+
+    <div scroll-active="" class="block block-logos py-small">
+        <div class="container">
+            <?=site()->homePage()->wysiwyg()->field("vars.logos")
+                ->listJson("blocks/block-logos/logos/logo-link-item")
+                ->horizontal()
+                ->contextMenuPosition(POSITION_BOTTOM)
+                ->blockPickerEmptyMessage("Insérez des logos dans cette liste")
+                ->contextMenuSize(SIZE_SMALL)
+                ->onlyImages()
+                ->htmlTag("div")
+                ->addClass("wrap")
+            ?>
+        </div>
+    </div>
+
     <?=$view->render("main-menu/main-menu")?>
 </footer>
