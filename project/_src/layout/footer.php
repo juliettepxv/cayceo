1fdffd3a55ead0f6a09a44bb632028d8a7845ff8
david <d.marsalone@gmail.com>
2020-03-02 06:46:23 +0100

    simplification footer

--- project/_src/layout/footer.php
+++ project/_src/layout/footer.php
@@ -1,22 +1,8 @@
 <?php
-
+/**
+ * Le footer
+ */
 ?>
 <footer id="footer">
-
-    <div scroll-active="" class="block block-logos pt-big pb-small">
-        <div class="container">
-            <?=site()->homePage()->wysiwyg()->field("vars.logos")
-                ->listJson("blocks/block-logos/logos/logo-link-item")
-                ->horizontal()
-                ->contextMenuPosition(POSITION_BOTTOM)
-                ->blockPickerEmptyMessage("Insérez des logos dans cette liste")
-                ->contextMenuSize(SIZE_SMALL)
-                ->onlyImages()
-                ->htmlTag("div")
-                ->addClass("wrap")
-            ?>
-        </div>
-    </div>
-
     <?=$view->render("main-menu/main-menu")?>
 </footer>
