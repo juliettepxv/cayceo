7bd31c61be236efdec9fa11b6f7ea5972d8b8c27
juliette <juliette.salomon@gmail.com>
2020-04-12 19:35:59 +0200

    cmmit route +db

--- configs/routes/localhost/github/cayceo.php
+++ configs/routes/localhost/github/cayceo.php
@@ -1,6 +1,6 @@
 <?php
-the()->fmkHttpRoot="/github/mouchard";// /github/classiq-startup
-the()->configProjectUrl=new \Pov\Configs\ProjectUrl("localhost/github/mouchard"); // localhost/github/classiq-startup/fr
+the()->fmkHttpRoot="/github/cayceo";// /github/classiq-startup
+the()->configProjectUrl=new \Pov\Configs\ProjectUrl("localhost/github/cayceo"); // localhost/github/classiq-startup/fr
 the()->fileSystem=new \Pov\Configs\FileSystem("project");
 the()->configProjectUrl->seoActive=true;
 the()->boot->loadProject("project");
