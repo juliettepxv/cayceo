c28d02c5c9c5465c63e52d02663d49be0e5ed082
david <d.marsalone@gmail.com>
2020-04-12 20:58:25 +0200

    lottie chargement d'image dynamique

--- project/_src/lottie/LottieLoader.js
+++ project/_src/lottie/LottieLoader.js
@@ -9,35 +9,59 @@ window.lottie=require("lottie-web");
  */
 export default class LottieLoader {
     constructor($main){
+        this.$main=$main;
+        let me=this;
         let url=$main.attr("lottie-url");
-        let autoplay=$main.attr("lottie-autoplay") === "true";
+        this.autoplay=$main.attr("lottie-autoplay") === "true";
         if(utils.device.isEdge){
-            autoplay=false;
+            this.autoplay=false;
         }
-        let loop=$main.attr("lottie-loop") === "true";
-        let preserveAspectRatio=$main.attr("lottie-preserve-aspect-ratio");
+        this.loop=$main.attr("lottie-loop") === "true";
+        this.preserveAspectRatio=$main.attr("lottie-preserve-aspect-ratio");
+        this.img=$main.attr("lottie-img");
+
+        //charge le json dynamiquement
+        $.getJSON(url, function(json) {
+            console.log(json)
+           me.animationData=json;
+            if(me.img){
+                me.animationData.assets[0]["p"]=me.img;
+                me.animationData.assets[0]["u"]=document.location.host;
+                me.animationData.assets[0]["u"]="";
+                console.log(me.animationData.assets[0]["u"])
+                console.log("lottie img",me.img)
+            }
+           me.buildLottie();
+        });
 
         //clean attributes
         $main.removeAttr("lottie-url")
             .removeAttr("lottie-autoplay")
             .removeAttr("lottie-loop")
             .removeAttr("lottie-preserve-aspect-ratio");
+
+    }
+
+    buildLottie(){
         let opt={
-            container: $main.get(0), // the dom element that will contain the animation
+            assetsPath:"",
+            container: this.$main.get(0), // the dom element that will contain the animation
             renderer: 'svg',
-            loop: loop,
-            autoplay: autoplay,
-            path: url // the path to the animation json
+            loop: this.loop,
+            autoplay: this.autoplay,
+            //path: url // the path to the animation json
+            animationData:this.animationData
         };
-        if(preserveAspectRatio){
+        if(this.preserveAspectRatio){
             opt.rendererSettings={
-                preserveAspectRatio:preserveAspectRatio
+                preserveAspectRatio:this.preserveAspectRatio
             }
         }
         let anim=lottie.loadAnimation(opt);
 
-        $main.data("lottie",anim);
+        this.$main.data("lottie",anim);
     }
+
     static initFromDom(){
         $body.find("[lottie-loader='']").each(function () {
             $(this).attr("lottie-loader","init");
