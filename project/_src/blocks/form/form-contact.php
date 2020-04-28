b56ea4bebb82d216558ca341042629ce05f0b42f
juliette <juliette.salomon@gmail.com>
2020-04-28 10:21:30 +0200

    micormodif formuliare +regie

--- project/_src/blocks/form/form-contact.php
+++ project/_src/blocks/form/form-contact.php
@@ -4,19 +4,20 @@
 <form data-form class="form-text">
 
 
-    <input  class="" type="text" placeholder="Nom" name="lastname">
+    <input  class="" type="text" placeholder="Nom*" name="lastname">
     <hr>
-    <input m class="" type="email"  placeholder="email" name="email1">
-    <hr>
-    <input m class="" type="tel"  placeholder="Téléphone" name="phone">
+    <input m class="" type="email"  placeholder="email*" name="email1">
+
     <hr>
     <span class="my-small"><?=trad("Demande de devis/démo/information*")?></span>
     <hr>
     <textarea  rows="10" class="resizeinput" name="message" placeholder="Décrivez nous votre besoin, nous nous chargerons d'y répondre au plus vite."></textarea>
     <hr>
-    <span class="my-small"><?=trad("Souhaitez vous être rappeler ?")?></span>
+    <span class="my-small"><?=trad("Souhaitez vous être rappelé ?")?></span>
     <hr>
     <input m class="" type="text"  placeholder="Par exemple : Lundi 12 Avril à 14h00" name="email1">
+    <hr>
+    <input m class="" type="tel"  placeholder="Téléphone" name="phone">
     <hr>
     <button class="mt-medium button sz-big" type="submit">Envoyer</button>
 
